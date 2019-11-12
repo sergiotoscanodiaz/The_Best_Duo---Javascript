9453fe8afb5127c94294221b8c9728950a45f516
Rodrigo López Ramos <55782190+rodrigolopezramoss@users.noreply.github.com>
2019-11-12 10:32:27 +0100

    Ejercicio 03
    Array aleatorio

---
+++ Array aleatorio/script.js
@@ -0,0 +1,33 @@
+var num = [20];
+
+
+num[0]= ((Math.random()*100 )+ 1);
+num[1]= ((Math.random()*100 )+ 1);
+num[2]= ((Math.random()*100 )+ 1);
+num[3]= ((Math.random()*100 )+ 1);
+num[4]= ((Math.random()*100 )+ 1);
+num[5]= ((Math.random()*100 )+ 1);
+num[6]= ((Math.random()*100 )+ 1);
+num[7]= ((Math.random()*100 )+ 1);
+num[8]= ((Math.random()*100 )+ 1);
+num[9]= ((Math.random()*100 )+ 1);
+num[10]= ((Math.random()*100 )+ 1);
+num[11]= ((Math.random()*100 )+ 1);
+num[12]= ((Math.random()*100 )+ 1);
+num[13]= ((Math.random()*100 )+ 1);
+num[14]= ((Math.random()*100 )+ 1);
+num[15]= ((Math.random()*100 )+ 1);
+num[16]= ((Math.random()*100 )+ 1);
+num[17]= ((Math.random()*100 )+ 1);
+num[18]= ((Math.random()*100 )+ 1);
+num[19]= ((Math.random()*100 )+ 1);
+
+for (var k = 0;k <= 19;k++){
+    document.write("Número " + (k +1) +": " + parseInt(num[k]));
+
+    document.write("<br>");
+}
+
+if (parseInt(num[k])) {
+    
+}
